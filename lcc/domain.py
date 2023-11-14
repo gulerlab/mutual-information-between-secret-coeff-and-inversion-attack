8888ad99db5c4ee8832bdf50f5a4891e386703da
umityigitbsrn <umityigitbsrn@gmail.com>
2023-11-13 18:57:04 -0800

    basic dresfl experiment is implemented with neural estimators and exhaustive search with no relationship between data and label

--- lcc/domain.py
+++ lcc/domain.py
@@ -30,6 +30,7 @@ def create_lcc_domain(K, data_range, T, prime, beta_arr):
 
 
 def create_lcc_gradient_domain_basic_setup(data_range, beta_arr, alpha_arr, weight, prime):
+    # this is when the relationship between x and y is linear
     # point-wise gradient calculation for linear regression for size 1
     # K, T = 1
     # g = -2(xy - x^2w)
@@ -53,6 +54,55 @@ def create_lcc_gradient_domain_basic_setup(data_range, beta_arr, alpha_arr, weig
         secret_random = list(secret_random)
         label = secret_random[0]
         domain[idx, 0], domain[idx, 1], domain[idx, 2], domain[idx, 3] = secret_random[0], secret_random[1], label, secret_random[2]
+
+        secret_data_pol = InterpolatedPoly(domain[idx, :2], beta_arr, prime)
+        secret_label_pol = InterpolatedPoly(domain[idx, 2:4], beta_arr, prime)
+
+        encoded_data_01 = secret_data_pol(alpha_arr[0])
+        encoded_data_02 = secret_data_pol(alpha_arr[1])
+        encoded_data_03 = secret_data_pol(alpha_arr[2])
+
+        encoded_label_01 = secret_label_pol(alpha_arr[0])
+        encoded_label_02 = secret_label_pol(alpha_arr[1])
+        encoded_label_03 = secret_label_pol(alpha_arr[2])
+
+        encoded_gradient_01 = calculate_gradient(encoded_data_01, encoded_label_01)
+        encoded_gradient_02 = calculate_gradient(encoded_data_02, encoded_label_02)
+        encoded_gradient_03 = calculate_gradient(encoded_data_03, encoded_label_03)
+
+        gradient_pol = InterpolatedPoly([encoded_gradient_01, encoded_gradient_02, encoded_gradient_03],
+                                        alpha_arr, prime)
+        domain[idx, 4], domain[idx, 5] = gradient_pol(beta_arr[0]), gradient_pol(beta_arr[1])
+        domain[idx, 6:] = gradient_pol.coefficients
+    return domain
+
+
+def create_lcc_gradient_domain_basic_setup_no_relationship(data_range, beta_arr, alpha_arr, weight, prime):
+    # this is when the relationship between x and y is linear
+    # point-wise gradient calculation for linear regression for size 1
+    # K, T = 1
+    # g = -2(xy - x^2w)
+    # x = y
+    data_range_domain = np.arange(data_range)
+    data_range_domain = [data_range_domain for _ in range(2)]
+    random_range_domain = np.arange(prime)
+    random_range_domain = [random_range_domain for _ in range(2)]
+    secret_random_domain_generator = itertools.product(*data_range_domain, *random_range_domain)
+
+    domain_size = data_range * data_range * prime * prime
+    domain_feature_size = 3 * 2 + 3
+    domain = np.empty((domain_size, domain_feature_size), dtype=int)
+
+    def calculate_gradient(encoded_data, encoded_label):
+        gradient = (encoded_label - encoded_data * weight) % prime
+        gradient = (encoded_data * gradient) % prime
+        gradient = (-2 * gradient) % prime
+        return gradient
+
+    for idx, secret_random in enumerate(secret_random_domain_generator):
+        secret_random = list(secret_random)
+        domain[idx, 0], domain[idx, 1], domain[idx, 2], domain[idx, 3] = (secret_random[0], secret_random[2],
+                                                                          secret_random[1], secret_random[3])
 
         secret_data_pol = InterpolatedPoly(domain[idx, :2], beta_arr, prime)
         secret_label_pol = InterpolatedPoly(domain[idx, 2:4], beta_arr, prime)
