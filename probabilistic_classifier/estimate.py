f451bcfb7ca83e9c97c818f6a7430ec7563d256f
umityigitbsrn <umityigitbsrn@gmail.com>
2023-11-20 00:53:23 -0800

    enough for today it is not working until now and I am a bit depressed

--- probabilistic_classifier/estimate.py
+++ probabilistic_classifier/estimate.py
@@ -41,8 +41,10 @@ def estimate_mi_for_multiclass_classification(model, joint_data, num_of_joint_tr
         class_distribution_trained = ((num_of_marginal_y_joint_xz_trained + num_of_marginal_x_joint_yz_trained) /
                                       (num_of_joint_trained + num_of_all_marginal_trained))
 
-        pointwise_dependency_joint = torch.log(torch.div(joint_prob[:, 0] * joint_prob[:, 1],
-                                                         joint_prob[:, 2] * joint_prob[:, 3]) *
-                                               class_distribution_trained)
+        pointwise_dependency = joint_prob[:, 0] * joint_prob[:, 1]
+        pointwise_dependency = torch.div(pointwise_dependency, joint_prob[:, 2])
+        pointwise_dependency = torch.div(pointwise_dependency, joint_prob[:, 3])
+
+        pointwise_dependency_joint = torch.log(pointwise_dependency * class_distribution_trained)
         curr_ldr = torch.sum(pointwise_dependency_joint) / pointwise_dependency_joint.size(0)
         return curr_ldr
